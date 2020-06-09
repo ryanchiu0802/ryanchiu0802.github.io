c4fe1bb426061e5083242a3ae88fee4883e1923f
ryanchiu0802 <63247952+ryanchiu0802@users.noreply.github.com>
2020-06-09 23:22:09 +0800

    Update Mine.Code.js

--- Mine.Code.js
+++ Mine.Code.js
@@ -13,7 +13,25 @@ var is_begin = false;	//is game started?
 var is_end = false;	//is game ended?
 var is_first_click = false; //is first block opened
 var oMainFrame;	//game main frame
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 //initialize the mine area
 function InitMineArea(row,col,mine_num,mine_index)
 {
